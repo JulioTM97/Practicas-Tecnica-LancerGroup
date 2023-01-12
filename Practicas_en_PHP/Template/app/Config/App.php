25fe6e98498b040c9d0a2a979db151ba8aa3b7cf
Julio Cesar Tapia Moya <julio.tapiamoya@gmail.com>
2023-01-12 13:52:01 -0400

    Se agrega una estructura basica al proyecto (header y footer). Además se agrega una constante que contiene la ruta base para un mejor acceso y posibles modificaciones.

--- Practicas_en_PHP/Template/app/Config/App.php
+++ Practicas_en_PHP/Template/app/Config/App.php
@@ -22,7 +22,7 @@ class App extends BaseConfig
      * explicitly and never rely on auto-guessing, especially in production
      * environments.
      */
-    public string $baseURL = 'http://localhost/Prueba_Tecnica_Lancer/Practicas_en_PHP/Template';
+    public string $baseURL = BASE_URL;
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
---
+++ Practicas_en_PHP/Template/app/Views/footer.php
@@ -0,0 +1,7 @@
+    <footer class="fixed-bottom bg-light">
+      <div class="container text-center p-3">
+        Desarrollado por:
+        <a class="text-dark" href="https://github.com/JulioTM97">Julio César Tapia Moya</a>
+      </div>
+    </footer>
+</html>
